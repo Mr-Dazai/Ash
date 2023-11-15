e1731df3193dac71bd43d5a1486189b80fa1d9ed
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-11-15 14:33:23 -0500

    chat loop

--- app.py
+++ app.py
@@ -8,14 +8,29 @@ def main():
     openai=OpenAI()
     API_KEY = os.getenv("OPENAI_API_KEY")
     openai.api_key=API_KEY
-    responce= openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role":"user","content":"What is Python?"}
-        ]
-    )
-    message_content = responce.choices[0].message.content
-    print(message_content)
+    chat_log=[]
+    while True:
+        user_message=input()
+        if user_message.lower()=='quit':
+            break
+        else:
+            chat_log.append({"role":"user","content":user_message})
+            responce= openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=chat_log
+            )
+            message_content = responce.choices[0].message.content
+            print(message_content)
+            chat_log.append({"role":"system","content":message_content})
+
+    # responce= openai.chat.completions.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role":"user","content":"What is Python?"}
+    #     ]
+    # )
+    # message_content = responce.choices[0].message.content
+    # print(message_content)
 
 
 if __name__=='__main__':
