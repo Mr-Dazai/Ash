4ddc4e7a45b23d3b20a05f400c08dcbc28c89f1c
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-11-15 17:29:37 -0500

    Simple UI

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
 import os
 from openai import OpenAI
-
+import tkinter as tk
+from tkinter import ttk
 from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    openai=OpenAI()
+    openai = OpenAI()
     API_KEY = os.getenv("OPENAI_API_KEY")
-    openai.api_key=API_KEY
-    chat_log=[]
-    while True:
-        user_message=input()
-        if user_message.lower()=='quit':
-            break
-        else:
-            chat_log.append({"role":"user","content":user_message})
-            responce= openai.chat.completions.create(
+    openai.api_key = API_KEY
+    chat_log = []
+
+    def search():
+        query = search_var.get()
+        text=("User: "+query)
+        print(text)
+        results.insert("", tk.END, values=(text,))
+        if query:
+            chat_log.append({"role": "user", "content": query})
+            response = openai.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=chat_log
             )
-            message_content = responce.choices[0].message.content
+            message_content = ("OpenAI: "+response.choices[0].message.content)
             print(message_content)
+            results.insert("", tk.END, values=(message_content,))
             chat_log.append({"role":"system","content":message_content})
 
-    # responce= openai.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[
-    #         {"role":"user","content":"What is Python?"}
-    #     ]
-    # )
-    # message_content = responce.choices[0].message.content
-    # print(message_content)
+    root = tk.Tk()
+    root.title("Chat GPT")
 
+    search_var = tk.StringVar()
+    search_box = ttk.Entry(root, textvariable=search_var)
+    search_box.pack(padx=10, pady=10, fill='x')
 
-if __name__=='__main__':
+    # search button
+    search_button = ttk.Button(root, text="Send", command=search)
+    search_button.pack(padx=10, pady=10)
+
+    results = ttk.Treeview(root, columns=('Chat'))
+    results.heading('Chat', text='Chat')
+    results['show'] = 'headings'
+    results.pack(padx=10, pady=10, fill='both', expand=True)
+
+    root.mainloop()
+
+if __name__ == '__main__':
     main()
